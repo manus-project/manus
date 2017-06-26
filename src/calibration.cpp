91b73dc697edd5d7f8da2e6fd70cc9ac91f88cc9
Luka Čehovin <luka.cehovin@gmail.com>
2017-06-26 21:56:32 +0200

    Random updates.

--- src/calibration.cpp
+++ src/calibration.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+float hardcoded_factors[] = {0.0009380807, 0.0009349769, 0.0009304994, 0.0008597145, 0.0008597145, 0.0008597145};
+
 typedef struct motor_calibration {
 	int min;
 	int max;
@@ -99,71 +101,119 @@ int main(int argc, char** argv) {
 
   	os.scanPortAutoAddServo();
 
-	motors.resize(n);
-	for (int i = 0; i < n; i++) {
-		motors[i].min = 1000000;
-		motors[i].max = -1000000;
-		motors[i].center = 0;
-		motors[i].factor = 0;
-		motors[i].addr = (int) adrs[i];
-		motors[i].id = -1;
-	}
+	if (argc < 3) {
 
-    /* get the terminal settings for stdin */
-    tcgetattr(STDIN_FILENO,&old_tio);
-    /* we want to keep the old setting to restore them a the end */
-    new_tio = old_tio;
-    /* disable canonical mode (buffered i/o) and local echo */
-    new_tio.c_lflag &=(~ICANON & ~ECHO);
-    /* set the new settings immediately */
-    tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
-
-	int m = 0;
-	while (m < n) {
-		cout << "Please move motor " << m << " to both extreme positions and then to center position and press any key." << endl;
-
-		while (wait_key() < 0) {
-			update_state(os, motors);
+		motors.resize(n);
+		for (int i = 0; i < n; i++) {
+			motors[i].min = 1000000;
+			motors[i].max = -1000000;
+			motors[i].center = 0;
+			motors[i].factor = 0;
+			motors[i].addr = (int) adrs[i];
+			motors[i].id = -1;
 		}
 
-		int candidate = -1;
-		int max_delta = 0;
+
+	    /* get the terminal settings for stdin */
+	    tcgetattr(STDIN_FILENO,&old_tio);
+	    /* we want to keep the old setting to restore them a the end */
+	    new_tio = old_tio;
+	    /* disable canonical mode (buffered i/o) and local echo */
+	    new_tio.c_lflag &=(~ICANON & ~ECHO);
+	    /* set the new settings immediately */
+	    tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+
+		int m = 0;
+		while (m < n) {
+			cout << "Please move motor " << m << " to both extreme positions and then to center position and press any key." << endl;
+
+			while (wait_key() < 0) {
+				update_state(os, motors);
+			}
+
+			int candidate = -1;
+			int max_delta = 0;
+
+			for (int i = 0; i < n; i++) {
+				int delta = motors[i].delta;
+				motors[i].delta = 0;
+
+				if (motors[i].id > -1)
+					continue;
+
+				if (delta > max_delta) {
+					max_delta = delta;
+					candidate = i;
+				}
+			}
+
+			motors[candidate].id = m;
+			m++;
+		}
+
+	} else {
+
+	  YAML::Node doc = YAML::LoadFile(argv[2]);
+
+      cout << "Loaded " << doc.size() << endl;
+	  for (int i = 0; i < doc.size(); i++) {
+	    motor_calibration d;
+
+		d.id = i;
+	    d.addr = doc[i]["id"].as<int>();
+	    d.min = doc[i]["min"].as<int>();
+	    d.max = doc[i]["max"].as<int>();
+	    d.center = doc[i]["center"].as<int>();
+	    d.factor = doc[i]["factor"].as<float>();
+
+	    motors.push_back(d);
+	  }
+
+	}
+
+	cout << "Writing data to motors" << endl;
+	for (int i = 0; i < motors.size(); i++) {
+
+		cout << "Writing data to motor " << i << endl;
+		os.writeEnable(motors[i].addr);
+		os.setMaxSeek(motors[i].addr, motors[i].max);
+		os.setMinSeek(motors[i].addr, motors[i].min);
+		os.writeDisable(motors[i].addr);
+		os.registerSave(motors[i].addr);
+
+	}
+
+	cout << "Verifying ... " << endl;
+	os.updateServoValuesAllServo();
+
+	for (int i = 0; i < motors.size(); i++) {
+
+		sv s = os.getServo(motors[i].addr);
+		cout << "Motor " << motors[i].addr << " min: " << s.minseek << " max: " << s.maxseek << endl;
+
+	}
+
+	if (argc < 3) {
+
+	    /* restore the former settings */
+	    tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+
+		std::sort(motors.begin(), motors.end(), compare);
+
+		YAML::Node sequence;  // starts out as null
 
 		for (int i = 0; i < n; i++) {
-			int delta = motors[i].delta;
-			motors[i].delta = 0;
-
-			if (motors[i].id > -1)
-				continue;
-
-			if (delta > max_delta) {
-				max_delta = delta;
-				candidate = i;
-			}
+			YAML::Node motor;
+			motor["id"] = motors[i].addr;
+			motor["min"] = motors[i].min;
+			motor["max"] = motors[i].max;
+			motor["center"] = motors[i].center;
+			motor["factor"] = hardcoded_factors[i];
+			sequence.push_back(motor);
 		}
 
-		motors[candidate].id = m;
-		m++;
+		std::ofstream fout("calibration.yaml");
+		fout << sequence;
+
 	}
-
-    /* restore the former settings */
-    tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
-
-	std::sort(motors.begin(), motors.end(), compare);
-
-	YAML::Node sequence;  // starts out as null
-
-	for (int i = 0; i < n; i++) {
-		YAML::Node motor;
-		motor["id"] = motors[i].addr;
-		motor["min"] = motors[i].min;
-		motor["max"] = motors[i].max;
-		motor["center"] = motors[i].center;
-		motor["factor"] = motors[i].factor;
-		sequence.push_back(motor);
-	}
-
-	std::ofstream fout("calibration.yaml");
-	fout << sequence;
-
 }
